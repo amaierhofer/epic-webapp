ebe5010de2596a95c47c494fbe8d60aff1d30700
amaierhofer <a.maierhofer@gmail.com>
2011-05-06 15:35:36 +0200

    cleaning up ringit application page

--- public/javascripts/mobileapps/ringit.js
+++ public/javascripts/mobileapps/ringit.js
@@ -1,22 +1,11 @@
-function qrcode(url){
-	  return '<img src ="http://qrcode.kaywa.com/img.php?s=5&d='+url+'">';
-	}
-   
-jQuery(document).ready(function() {
 
-	var theqrcode = qrcode(app.xmpp('getUsername') + ':' + app.xmpp('getPassword') + '@' + app.xmpp('getServer'));
-	jQuery('#qrcode').append(theqrcode);
-	
-
-    jQuery("#ringitbutton").click(function() {
-	//iterate over all connected peers and send them a message
-	for (var peer in app.xmpp('state').peers) {
-		if(peer){
-                app.xmpp('sendEpicIntent', peer, 'org.epic.action.RingPhone', null, null);
-		}
-    }
-	
-	
-	
+$(function() {
+  $("#ring").click(function() {
+    var peers = app.xmpp('state').peers;
+    if(_.isEmpty(peers)) { $.nmk.log('no peers'); }
+    _.each(peers, function(peer) {
+      $.nmk.log('peer: ' + peer);
+      app.xmpp('sendEpicIntent', peer, 'org.epic.action.RingPhone', null, null);
     });
+  });
 });
