92f7b4ef7580be90f900ea1b4ce818796ff7ceae
amaierhofer <a.maierhofer@gmail.com>
2011-05-08 23:35:04 +0200

    some cleanups, support for enabling/disabling buttons

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,12 @@
 (function($) {
   $(function() {
     window.epic = $('body').app();
+    window.epic.bind('xmpp.transition', function(ev,peers) {
+      if(peers.length === 0) { return; }
+      var op = _.first(_.values(peers.peers)) == 'available' ? 'enable' : 'disable';
+      $('a[data-role]="button"').each(function() {
+        $(this).button().button(op);
+      });
+    });
   });
 })(jQuery);
